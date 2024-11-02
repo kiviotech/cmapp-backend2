d6b479e4e65180fc3af685bccd3e2faa126baf64
Adityasiingh14 <Adityasiingh14@gmail.com>
2024-11-02 14:00:40 +0530

    upload file fixed

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,29 +1,21 @@
-// config/middlewares.js
-module.exports = ({ env }) => [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
+module.exports = [
+  "strapi::errors",
+  "strapi::security",
+  "strapi::poweredBy", // here
   {
-    name: 'strapi::cors',
-    config: {
-      origin: ['https://cmapp.kivio.in/','http://localhost:8081','*'], // Add your Angular app URLs here
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    },
+    name: "strapi::cors",
+    // config: {
+    //   enabled: false,
+    //   origin: ["*"],
+    //   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
+    //   headers: ["Content-Type", "Authorization", "Origin", "Accept"],
+    //   keepHeaderOnError: true,
+    // },
   },
-  {
-    name: 'strapi::body',
-    config: {
-      formLimit: '2gb',  // Set form data size limit
-      jsonLimit: '2gb',  // Set JSON payload size limit
-      textLimit: '2gb',  // Set text payload size limit
-      formidable: {
-        maxFileSize: 2 * 1024 * 1024 * 1024 // Set max file size to 2GB
-      }
-    },
-  },
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
