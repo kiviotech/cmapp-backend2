b8ccf448ff3ee76eb9fc4295ae59a3f9c0ff89fd
root <root@e2e-101-241.ssdcloudindia.net>
2025-03-21 06:10:04 +0530

    chages

--- config/api.js
+++ config/api.js
@@ -1,7 +1,7 @@
 module.exports = {
   rest: {
-    defaultLimit: 100,
-    maxLimit: 100,
+    defaultLimit: 5,
+    maxLimit: 5,
     withCount: true,
   },
 };
--- config/middlewares.js
+++ config/middlewares.js
@@ -37,7 +37,7 @@ module.exports = ({ env }) => [
   {
     name: "strapi::cors",
     config: {
-      origin: ['https://cmapp.kivio.in',"http://localhost:5173", "http://localhost:5173"], // Add your Angular app URLs here
+      origin: ['https://zp1v56uxy8rdx5ypatb0ockcb9tr6a-oci3--5173--5a421e5b.local-credentialless.webcontainer-api.io','https://cmapp.kivio.in',"http://cmappweb.kivio.in","http://localhost:5173", "http://localhost:5173"], // Add your Angular app URLs here
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization'],
       keepHeaderOnError: true,
